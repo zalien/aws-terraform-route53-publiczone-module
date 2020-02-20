5861f511c1afc09c13666ebc522e8ab81469750c
Sérgio Teixeira <sergiopintoteixeira@gmail.com>
2020-02-20 12:46:49 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,8 +2,6 @@ variable "name" {}
 variable "tags" {
   type = "map"
   default = {
-    Terraform     = "true"
-    business-unit = "JMDS"
-    application   = "rds-postgres"
+    Terraform = "true"
   }
 }
